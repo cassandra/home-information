434be5a0407525c373e8daf7fa495b414fbc054f
Anthony Cassandra <github@cassandra.org>
2024-12-19 10:16:25 -0600

    Created simulator.py wrapper script.

--- src/hi/simulator/urls.py
+++ src/hi/simulator/urls.py
@@ -2,6 +2,6 @@ from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    re_path( r'^simulate$', views.simulate, name = 'simulator_home' ),
-    re_path( r'^setup$', views.setup, name = 'simulator_setup' ),
+    re_path( r'^$', views.simulate, name = 'home' ),
+    re_path( r'^setup$', views.setup, name = 'setup' ),
 ]
---
+++ src/simulator.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import os
+import sys
+from subprocess import run
+
+
+def main():
+    settings_module = 'hi.settings.simulator'
+    os.environ['DJANGO_SETTINGS_MODULE'] = settings_module
+
+    # Ensure manage.py is being used
+    manage_py_path = os.path.join( os.path.dirname(__file__), 'manage.py' )
+    if not os.path.exists( manage_py_path ):
+        print("Error: manage.py not found in the current directory.")
+        sys.exit(1)
+
+    if len(sys.argv) < 2:
+        print( "Usage: simulator.py <command> [options]" )
+        sys.exit(1)
+
+    # Different default port for simulator's "runserver"
+    command = sys.argv[1]
+    command_args = sys.argv[2:]
+    if command == "runserver":
+        if not any( arg.isdigit() for arg in command_args ):
+            command_args.append( "8001" )
+
+    full_command = [ 'python', manage_py_path, command ] + command_args + [ f'--settings={settings_module}' ]
+    result = run( full_command )
+    sys.exit( result.returncode )
+
+    
+if __name__ == "__main__":
+    main()
