dd85dc3d3c316fe00819f045c9370a9a04b094a4
Anthony Cassandra <github@cassandra.org>
2025-02-11 21:54:06 -0600

    Added to config internal and added ngnix service media files.

--- src/hi/settings/base.py
+++ src/hi/settings/base.py
@@ -16,7 +16,7 @@ import os
 from .helpers import EnvironmentSettings
 
 env_settings = EnvironmentSettings.get()
-    
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
