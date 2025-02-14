6c6578909836b577c2b9ab6507eef5997755c87a
Anthony Cassandra <github@cassandra.org>
2025-02-13 22:31:58 -0600

    Fixed env var initialization ordering issue.

--- src/hi/settings/ci.py
+++ src/hi/settings/ci.py
@@ -38,18 +38,3 @@ BASE_URL_FOR_EMAIL_LINKS = 'http:/127.0.0.1:8411/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 SUPPRESS_SELECT_REQUEST_ENPOINTS_LOGGING = True
 SUPPRESS_MONITORS = True
-
-# These added for CI but normally found from comand line Env Vars..
-SECRET_KEY = "django-insecure-@egpl51khoap^%x8*jc4xj0(hv)bar-%5897+@h665k3txo))7"
-DJANGO_SUPERUSER_EMAIL = 'foo@example.com'
-DJANGO_SUPERUSER_PASSWORD = 'foobar'
-ALLOWED_HOSTS = 'localhost'
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/tmp/hi.sqlite3',
-    }
-}
-MEDIA_ROOT = '/tmp'
-REDIS_HOST = 'localhost'
-REDIS_PORT = 6379
